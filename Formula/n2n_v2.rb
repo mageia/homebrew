622b5874d57ab0f1f4fb958fa4c49fbc898f6e49
Mageia <yzg963@gmail.com>
2020-03-25 13:22:53 +0800

    n2n_v2

--- Formula/n2n_v2.rb
+++ Formula/n2n_v2.rb
@@ -1,15 +1,16 @@
 class N2nV2 < Formula
   desc ""
   homepage ""
-  url "https://github.com/ntop/n2n", :using => :git, :tag => "2.6-stable"
-  # sha256 "d85843f2a3866d2dcf91c9eb948d2b6f192562a1e71ecd3d7ec51845a1a53435"
+  url "https://github.com/mageia/n2n", :using => :git, :tag => "2.6-stable"
 
   depends_on "cmake" => :build
   depends_on "openssl" => :build
 
   def install
+    system "mkdir", "build"
+    system "cd", "build"
+    system "cmake", "-DOPENSSL_ROOT_DIR=/usr/local/opt/openssl", ".."
     system "make"
-    system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
