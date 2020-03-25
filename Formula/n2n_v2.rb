ceaef0f33d0a8a03789df8ef3d507ba54301d2a8
Mageia <yzg963@gmail.com>
2020-03-25 13:11:55 +0800

    n2n_v2

--- Formula/n2n_v2.rb
+++ Formula/n2n_v2.rb
@@ -1,8 +1,8 @@
 class N2nV2 < Formula
   desc ""
   homepage ""
-  url "https://github.com/mageia/n2n_v2/archive/v2.1.0.tar.gz"
-  sha256 "d85843f2a3866d2dcf91c9eb948d2b6f192562a1e71ecd3d7ec51845a1a53435"
+  url "https://github.com/ntop/n2n", :using => :git, :tag => "2.6-stable"
+  # sha256 "d85843f2a3866d2dcf91c9eb948d2b6f192562a1e71ecd3d7ec51845a1a53435"
 
   depends_on "cmake" => :build
   depends_on "openssl" => :build
